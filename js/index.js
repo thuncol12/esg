3bb700b4e554f5393ebfbd34fd5753588b0248f0
김정례 <thuncol12@naver.com>
2023-07-13 17:29:14 +0900

    js mouseenter event시 색상 변경 실습

--- js/index.js
+++ js/index.js
@@ -29,3 +29,18 @@ function handleClick() {
 }
 
 title.addEventListener("click", handleClick);
+
+// 배열만들었따고해도 랜덤으로 0123 이라는값을 가져올수있어야됨
+// 랜덤 정수 뽑는 함수 구글링
+// 컬러가 4개 이상 들어가있는 배열을 만들고
+// 특정 이벤트를 통해 랜덤으로 컬러가 선택되도록 마들어보세요.
+const colors = ["#bad0ff", "#e35d6a", "#bcb3f8", "#339933"];
+const backgroundColors = ["#fd8270", "#1974d2", "#f8d458", "#f7c0dc"];
+function handleMouseEnter() {
+  const i = Math.floor((Math.random() * 10) % 3);
+  const j = Math.floor((Math.random() * 10) % 3);
+
+  title.style.color = colors[i];
+  title.style.backgroundColor = backgroundColors[j];
+}
+title.addEventListener("mouseenter", handleMouseEnter);
