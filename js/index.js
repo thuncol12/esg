20000a91a7eb4c458843a1233c1b28c46411d0e0
김정례 <thuncol12@naver.com>
2023-07-13 17:13:43 +0900

    js text 색상 변경 함수

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let title = document.getElementById("title");
 console.log(title.innerText);
 console.log(title.style);
-title.style = "color: #bad0ff";
 
 title.innerHTML = "hello javascript";
 
@@ -16,3 +15,17 @@ function notifyResize(event) {
 
 // 2번째 인자 함수에 괄호 안붙임!!
 window.addEventListener("resize", notifyResize);
+
+const BASE_COLOR = "orange";
+const OTHER_COLOR = "green";
+
+function handleClick() {
+  let currentColor = title.style.color;
+  if (currentColor == BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+title.addEventListener("click", handleClick);
