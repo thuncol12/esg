15bd1711139931ca637100fc691ce5c420439568
김정례 <thuncol12@naver.com>
2023-07-13 11:43:50 +0900

    js 객체와 함수 실습

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// const: 상수 선언. let: 변수 선언.
 const a = 30;
 let b = 20;
 b = a;
@@ -8,6 +9,7 @@ console.log(a + b);
 let date = new Date();
 let hour = date.getHours();
 
+// if-else문
 if (hour < 11) {
   alert("아침");
 } else if (hour < 15) {
@@ -16,6 +18,7 @@ if (hour < 11) {
   alert("저녁");
 }
 
+// switch문
 let input = Number(prompt("숫자를 입력하세요.", "숫자"));
 switch (input % 2) {
   case 0:
@@ -23,6 +26,31 @@ switch (input % 2) {
     break;
   case 1:
     alert("홀수입니다.");
+    break;
   default:
     alert("숫자가 아닙니다.");
 }
+
+// 객체 내에 함수 선언 가능.
+const person = {
+  greet: function () {
+    return "hello";
+  },
+};
+console.log(person.greet());
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  mult: function (a, b) {
+    return a + b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+};
+console.log(calculator.div(4, 2));
