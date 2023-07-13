cee777468540f750cea63f46ec1ecf25069630a3
김정례 <thuncol12@naver.com>
2023-07-13 17:03:20 +0900

    js html css 연동 실습

--- js/index.js
+++ js/index.js
@@ -1,76 +1,18 @@
-// const: 상수 선언. let: 변수 선언.
-const a = 30;
-let b = 20;
-b = a;
+let title = document.getElementById("title");
+console.log(title.innerText);
+console.log(title.style);
+title.style = "color: #bad0ff";
 
-console.log(a);
-console.log(a + b);
+title.innerHTML = "hello javascript";
 
-let date = new Date();
-let hour = date.getHours();
+function notifyResize(event) {
+  console.log(event);
+  console.log("resized");
 
-// 조건문
-// if-else문
-if (hour < 11) {
-  alert("아침");
-} else if (hour < 15) {
-  alert("점심");
-} else {
-  alert("저녁");
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  console.log(width, height);
 }
 
-// switch문
-let input = Number(prompt("숫자를 입력하세요.", "숫자"));
-switch (input % 2) {
-  case 0:
-    alert("짝수입니다.");
-    break;
-  case 1:
-    alert("홀수입니다.");
-    break;
-  default:
-    alert("숫자가 아닙니다.");
-}
-
-// 객체 내에 함수 선언 가능.
-const person = {
-  greet: function () {
-    return "hello";
-  },
-};
-console.log(person.greet());
-
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  mult: function (a, b) {
-    return a + b;
-  },
-  div: function (a, b) {
-    return a / b;
-  },
-};
-console.log(calculator.div(4, 2));
-
-// 반복문
-let array = ["포도", "딸기", "당근", "바나나", "망고"];
-
-// for문
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
-}
-
-// for-in문 -> 인덱스 반환
-for (let i in array) {
-  console.log(i);
-  console.log(array[i]);
-}
-
-// for-of문 -> 각 요소 반환
-for (let a of array) {
-  console.log(a);
-}
+// 2번째 인자 함수에 괄호 안붙임!!
+window.addEventListener("resize", notifyResize);
--- js/practice.js
+++ js/practice.js
@@ -1,3 +1,80 @@
+// const: 상수 선언. let: 변수 선언.
+const a = 30;
+let b = 20;
+b = a;
+
+console.log(a);
+console.log(a + b);
+
+let date = new Date();
+let hour = date.getHours();
+
+// 조건문
+// if-else문
+if (hour < 11) {
+  alert("아침");
+} else if (hour < 15) {
+  alert("점심");
+} else {
+  alert("저녁");
+}
+
+// switch문
+let input = Number(prompt("숫자를 입력하세요.", "숫자"));
+switch (input % 2) {
+  case 0:
+    alert("짝수입니다.");
+    break;
+  case 1:
+    alert("홀수입니다.");
+    break;
+  default:
+    alert("숫자가 아닙니다.");
+}
+
+// 객체 내에 함수 선언 가능.
+const person = {
+  greet: function () {
+    return "hello";
+  },
+};
+console.log(person.greet());
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  mult: function (a, b) {
+    return a + b;
+  },
+  div: function (a, b) {
+    return a / b;
+  },
+};
+console.log(calculator.div(4, 2));
+
+// 반복문
+let array = ["포도", "딸기", "당근", "바나나", "망고"];
+
+// for문
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+// for-in문 -> 인덱스 반환
+for (let i in array) {
+  console.log(i);
+  console.log(array[i]);
+}
+
+// for-of문 -> 각 요소 반환
+for (let a of array) {
+  console.log(a);
+}
+
 // 1.
 // 오브젝트 내에 인자를 받는 함수를 만들어서 짝수인지 홀수인지 판별
 obj = {
